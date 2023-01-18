95c5ab1b7ea9c5656a1d2a97630b455aaf50e178
Emile <bvcxzq1@gmail.com>
2023-01-18 22:17:34 +0000

    Add js code for division.

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,8 @@ if(gameType === "addition"){
     displayMultiplyQuestion(num1, num2);
 } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+} else if (gameType === "division") {
+    displayDivisionyQuestion(num1, num2);
 } else {
     alert(`Unknow game type: ${gameType}`);
     throw `Unknow game type: ${gameType}. Aborting!`;
@@ -88,6 +90,8 @@ function calculateCorrectAnswer () {
         return [operand1 * operand2, "multiply"]
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"]
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"]
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -132,5 +136,13 @@ function displayMultiplyQuestion (operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "X";
+
+}
+
+function displayDivisionyQuestion (operand1, operand2) {
+    //document.getElementById('operand1').textContent = operand1 + (operand2 - (operand1 % operand2)) % operand2; Works sortof
+    document.getElementById('operand1').textContent = operand1*operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
